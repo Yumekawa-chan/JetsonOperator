7987a98ee57ba1f1db82f4a69a59f09fb946e515
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2023-10-30 18:13:32 +0900

    add src

---
+++ operateJetsonCamera.py
@@ -0,0 +1,25 @@
+# jetson１台分なので，複数台に対応させる必要あり．
+import socket
+import threading
+
+
+def wait_for_key():
+    input("Press Enter to send capture command...")
+    for client in clients:
+        client.sendall(b'capture')
+
+
+def accept_clients():
+    while True:
+        client, addr = server.accept()
+        clients.append(client)
+        print(f'Connected by {addr}')
+
+
+clients = []
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', 65432))  # Bind to all available interfaces, port 65432
+server.listen()
+
+threading.Thread(target=accept_clients).start()
+wait_for_key()
