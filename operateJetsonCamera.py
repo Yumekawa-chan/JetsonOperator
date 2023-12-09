2425c7f904ba96f4c7911a3b8b724f2b450578c5
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2023-12-09 12:07:27 +0900

    update

--- operateJetsonCamera.py
+++ operateJetsonCamera.py
@@ -4,7 +4,6 @@ import os
 
 def receive_and_save_image(client_socket):
     while True:
-        # ファイル名を受け取る
         filename = ""
         while True:
             chunk = client_socket.recv(1024)
@@ -13,23 +12,17 @@ def receive_and_save_image(client_socket):
                 filename = filename.split('\n')[0]
                 break
 
-        # imagesフォルダがなければ作成
         if not os.path.exists('images'):
             os.makedirs('images')
 
-        # 画像データを受け取る
-        # 画像データを受け取る
         with open(os.path.join('images', filename), 'wb') as file:
             while True:
                 data = client_socket.recv(4096)
                 if b'ENDOFIMAGE' in data:
-                    # ENDOFIMAGE マーカーの前までのデータを書き込む
                     end_index = data.index(b'ENDOFIMAGE')
                     file.write(data[:end_index])
                     break
                 file.write(data)
-
-
 
 def send_capture_command():
     while True:
@@ -42,7 +35,6 @@ def accept_clients():
         client, addr = server.accept()
         clients.append(client)
         print(f'Connected by {addr}')
-        # 画像受信用のスレッドを開始（ここでclient_socketを渡す）
         threading.Thread(target=receive_and_save_image, args=(client,), daemon=True).start()
 
 clients = []
@@ -50,6 +42,5 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 65432))
 server.listen()
 
-# accept_clientsをスレッドで実行
 threading.Thread(target=accept_clients, daemon=True).start()
 send_capture_command()
